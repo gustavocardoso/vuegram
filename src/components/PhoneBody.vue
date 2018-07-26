13d21112bb3758518d8492825337b9b1664adca1
Gustavo Cardoso <gustavocardoso@gmail.com>
2018-07-26 03:45:58 +0000

    Post screen

---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="phone-body">
+    <div class="feed">
+      <vuegram-post v-for="post in posts"
+        :post="post"
+        :key="posts.indexOf(post)">
+      </vuegram-post>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from './Post'
+
+export default {
+  name: 'PhoneBody',
+
+  components: {
+    'vuegram-post': Post
+  },
+
+  props: {
+    posts: Array,
+    filters: Array
+  }
+}
+</script>
+
+<style lang="scss" src="../assets/scss/phone-body.scss">
+</style>
